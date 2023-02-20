5d99a030ac86f5414052af8f5170b9a012c1a442
ErikaSalamanca <124635427+ErikaSalamanca@users.noreply.github.com>
2023-02-20 18:15:21 -0500

    Diseño del componente ingresar
    Se diseño y se agregó el formulario para ingreso.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PeliculasComponent } from './routes/peliculas/peliculas.component';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './routes/inicio/inicio.component';
 import { SeriesComponent } from './routes/series/series.component';
 import { IngresarComponent } from './routes/ingresar/ingresar.component';
+import { IniciarsesionComponent } from './routes/ingresar/iniciarsesion/iniciarsesion.component';
+import { RegistrarseComponent } from './routes/ingresar/registrarse/registrarse.component';
 
 
 const routes: Routes=[
@@ -30,6 +32,14 @@ const routes: Routes=[
   {
     path: '**',
     component: InicioComponent
+  },
+  {
+    path: 'iniciarsesion',
+    component: IniciarsesionComponent
+  },
+  {
+    path: 'registrarse',
+    component: RegistrarseComponent
   }
 
 
